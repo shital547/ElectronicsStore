06b2a2f52db48e942a1abd6368908c7356edc80c
shitaldixit547 <sbdixit547@gmail.com>
2023-03-31 22:14:31 +0530

    first commit for project

---
+++ src/main/java/com/bikkadit/electronicsstore/dtos/PageableResponse.java
@@ -0,0 +1,26 @@
+package com.bikkadit.electronicsstore.dtos;
+
+import lombok.*;
+
+import java.util.List;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PageableResponse<T> {
+
+    private List<T> content;
+    private int pageNumber;
+
+    private int pageSize;
+
+    private long totalElements;
+
+    private int totalPages;
+
+    private boolean lastPage;
+
+}
