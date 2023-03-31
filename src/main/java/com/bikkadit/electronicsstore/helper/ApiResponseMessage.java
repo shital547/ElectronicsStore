31ce4f9b3440b1ccbab4c6ea9ee93bc8225a64b5
shitaldixit547 <sbdixit547@gmail.com>
2023-03-31 16:16:54 +0530

    file service completed

---
+++ src/main/java/com/bikkadit/electronicsstore/helper/ApiResponseMessage.java
@@ -0,0 +1,21 @@
+package com.bikkadit.electronicsstore.helper;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Setter
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public class ApiResponseMessage {
+
+        private String message;
+
+        private boolean success;
+
+        private HttpStatus status;
+
+    }
+
+
