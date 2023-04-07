f8c86c87044a6fc4d99a94e00e7dec51925d5ffe
shitaldixit547 <sbdixit547@gmail.com>
2023-04-07 17:08:06 +0530

    App Constant completed

---
+++ src/main/java/com/bikkadit/electronicsstore/config/AppConstants.java
@@ -0,0 +1,17 @@
+package com.bikkadit.electronicsstore.config;
+
+public class AppConstants {
+    public static final String USER = "user not found with given id!!";
+
+    public static final String USER_ID = "userId";
+
+    public static final String USER_EMAIL = "user not found with given id and password";
+
+    public static final String DELETE_USER = "user delete Successfully";
+
+    public static final String YES = "Y";
+
+    public static final String NO = "N";
+}
+
+
